c37ce3bea42eddbbb3c8ca3cfc3e0e47bfb622f5
paleviews <lyatqh@hotmail.com>
2023-04-30 00:16:07 +0800

    Update the comments

--- group.go
+++ group.go
@@ -1,3 +1,6 @@
+// Package rungroup provides a clean and composable API for implementing
+// a common concurrency pattern to run, cancel and wait for multiple goroutines
+// as a group.
 package rungroup
 
 import (
@@ -8,15 +11,36 @@ import (
 
 type GoFunc func(context.Context)
 
+// Group managers the lifecycle of multiple goroutines as a group.
+//
+// A Group transitions from an active state to a canceled state,
+// and finally to a closed state.
+//
+// Upon instantiation, a Group resides in an active state.
+// An active Group can spawn a goroutine and track its progress.
+//
+// After the Cancel method is called, the Group enters a canceled state.
+// A canceled Group ceases to accept new goroutines.
+//
+// Once all the goroutines have exited, the Group transitions to a closed state
+// and the Wait exits to unblock the caller of Wait.
+//
+// Note that if all the goroutines within a Group exit and Cancel remains
+// uncalled, the Group will advance from an active state to a closed state,
+// bypassing the canceled phase.
 type Group interface {
-	// Go tries to add f to the Group and then run f in a separated goroutine immediately.
-	// It returns a bool to indicate whether the try succeeded.
+	// Go attempts call function f in a new goroutine and add f to the Group.
+	// It returns a boolean to indicate whether the attempt was successful.
+	// The context provided to f as a function argument will be canceled
+	// (i.e. the Done channel of the context will be closed) once the Cancel
+	// is invoked, so f can react accordingly.
 	Go(f GoFunc) bool
-	// Cancel cancels the context.
-	// Thus sending exit signals to all goroutines in the Group,
-	// but does not wait for them to exit.
+	// Cancel cancels the underlying context, effectively sending exit signals
+	// to all goroutines in the Group, but does not wait for them to exit.
+	// Cancel may be called by multiple goroutines simultaneously.
+	// After the first call, subsequent calls to a CancelFunc do nothing.
 	Cancel()
-	// Wait waits for all goroutines in the Group to exit.
+	// Wait blocks until all goroutines in the Group have exited.
 	Wait()
 }
 
@@ -27,24 +51,31 @@ type config struct {
 
 type Option func(*config)
 
+// WithTrapSignals configures the Group to intercept the OS signals
+// and Cancel the Group if it does receive the OS signal.
 func WithTrapSignals(signals []os.Signal) Option {
 	return func(c *config) {
 		c.trapSignals = signals
 	}
 }
 
+// WithPanicHandler wraps each function in the Group with a panic recovery
+// mechanism and passes the recovered object to function f if it does catch
+// a panic.
 func WithPanicHandler(f func(recovered interface{})) Option {
 	return func(c *config) {
 		c.panicHandler = f
 	}
 }
 
-func New(ctx context.Context, options ...Option) Group {
+// New creates a new Group. The underlying context of the returned Group
+// is based on the provided parent context.
+func New(parent context.Context, options ...Option) Group {
 	var cfg config
 	for _, opt := range options {
 		opt(&cfg)
 	}
-	newCtx, cancel := context.WithCancel(ctx)
+	newCtx, cancel := context.WithCancel(parent)
 	var runGroup Group = &basicGroup{
 		ctx:       newCtx,
 		cancel:    cancel,
